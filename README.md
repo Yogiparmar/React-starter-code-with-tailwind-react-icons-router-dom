1ffd87408971bd5f2d0f89d22771df0d2a8ff76c
yogi parmar <142466529+Yogiparmar@users.noreply.github.com>
2024-09-19 17:18:11 +0530

    React Starter Template With Tailwind CSS, React Icons & React Router Dom

---
+++ README.md
@@ -0,0 +1,7 @@
+# Vite Starter Template With React
+
+This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+
+- Tailwind CSS
+- React Router Dom
+- React Icons
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="size-full h-screen flex justify-center items-center">
+        <h1 className="text-4xl font-semibold text-center">
+          Welcome to the World of Possibilities
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
